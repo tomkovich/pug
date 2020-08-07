ca12b6b6f2305f3d410596585c6a88f677b8246f
tomkovich <bayern_munchen22@mail.ru>
2020-08-07 16:10:01 +0300

    Chapter 2

---
+++ public/js/scripts.js
@@ -0,0 +1,10 @@
+var slider = tns({
+  container: '.reviews-slider',
+  items: 2,
+  slideBy: 'page',
+  controls: false,
+  gutter: 70,
+  navPosition: 'bottom',
+  navContainer: ".reviews-slider_nav",
+  mouseDrag: true,
+});
